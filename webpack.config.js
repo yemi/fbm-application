3db0a2f2dd506b9a7f370675b8251933c1a72a22
Fabian <rocaboca@gmail.com>
2015-11-05 09:52:43 +0100

    Removed browserify and added webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path')
+
+module.exports = {
+  entry: [
+    path.join(__dirname, 'app/app.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  devtool: 'eval-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'app'),
+        loader: 'babel'
+      }
+    ]
+  }
+}
