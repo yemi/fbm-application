fc141bc85dc364f52f92aa27373be866dade87a6
Fabian Rios <rocaboca@gmail.com>
2015-11-09 11:15:59 +0100

    Some footer view refactor

--- app/Component/Footer/view.js
+++ app/Component/Footer/view.js
@@ -3,7 +3,7 @@ import {hJSX} from '@cycle/dom'
 import R from 'ramda'
 import {log, slash} from '../../utils'
 
-const getPathToStep = (targetStepIndex, props) => {
+const getUrlToStep = (targetStepIndex, props) => {
   const maxStepIndex = props.totalSteps - 1
   const minStepIndex = 0
   const safeStepIndex = targetStepIndex > props.activeStep
@@ -13,13 +13,7 @@ const getPathToStep = (targetStepIndex, props) => {
   const hasTargetStepIndex = R.compose(R.equals(safeStepIndex), R.prop('index'))
   const getStepPath = R.compose(R.head, R.keys, R.pickBy(R.both(isPageStep, hasTargetStepIndex)))
   const stepPath = getStepPath(props.pages)
-  return stepPath
-}
-
-const getPrevAndNextUrls = props => {
-  const prevStepUrl = slash(getPathToStep(props.activeStep - 1, props))
-  const nextStepUrl = slash(getPathToStep(props.activeStep + 1, props))
-  return {prevStepUrl, nextStepUrl}
+  return slash(stepPath)
 }
 
 const renderPostErrors = ({postErrors}) =>
@@ -36,44 +30,58 @@ const renderStepIndicator = props => {
   )
 }
 
-const getPrevLinkClasses = props => {
+const getPrevButtonClasses = props => {
   const hidden = props.activeStep === 0 ? 'hidden' : ''
   return `button button--secondary mh05 ${hidden}`
 }
 
-const getNextLinkClasses = props => {
+const getNextButtonClasses = props => {
   const disabled = props.canContinue ? '' : 'button--disabled'
   return `button ${disabled} mh05`
 }
 
-const activeStepIsLastStep = props => props.activeStep === props.totalSteps - 1
-
-const getNextLinkId = props => {
-  const id = activeStepIsLastStep(props) ? 'submit' : 'stepContinue'
-  return id
+const renderNextStepButton = props => {
+  const nextStepUrl = getUrlToStep(props.activeStep + 1, props)
+  const nextButtonClasses = getNextButtonClasses(props)
+  return (
+    <a href={nextStepUrl}
+       id="stepNext"
+       className={nextButtonClasses}>
+      Continue
+    </a>
+  )
 }
 
+const renderSubmitButton = props => {
+  const nextButtonClasses = getNextButtonClasses(props)
+  return (
+    <button id="submit" className={nextButtonClasses}>
+       Submit application
+    </button>
+  )
+}
+
+const activeStepIsLastStep = props => props.activeStep === props.totalSteps - 1
+
+const renderNextButton = props =>
+  activeStepIsLastStep(props) ? renderSubmitButton(props) : renderNextStepButton(props)
+
 const view = R.map(props => {
-  const {prevStepUrl, nextStepUrl} = getPrevAndNextUrls(props)
+  const prevStepUrl = getUrlToStep(props.activeStep - 1, props)
   const postErrors = true ? null : renderPostErrors(props) // TODO: Do post error check
   const stepIndicator = renderStepIndicator(props)
-  const nextLinkClasses = getNextLinkClasses(props)
-  const prevLinkClasses = getPrevLinkClasses(props)
-  const nextLinkId = getNextLinkId(props)
+  const prevButtonClasses = getPrevButtonClasses(props)
+  const nextButton = renderNextButton(props)
   return (
     <footer className="textCenter bgWhite pv20">
       {postErrors}
       <a href={prevStepUrl}
          id="stepBack"
-         className={prevLinkClasses}>
+         className={prevButtonClasses}>
         Back
       </a>
       {stepIndicator}
-      <a href={props.canContinue && R.not(activeStepIsLastStep(props)) ? nextStepUrl : '#'}
-         id={nextLinkId}
-         className={nextLinkClasses}>
-        Continue
-      </a>
+      {nextButton}
     </footer>
   )
 })
